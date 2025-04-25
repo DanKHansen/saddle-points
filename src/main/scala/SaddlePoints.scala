82487f9f57b27efd8d75e37aab6ef4cac4b62c3b
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-25 15:55:26 +0200

    refactor

--- src/main/scala/SaddlePoints.scala
+++ src/main/scala/SaddlePoints.scala
@@ -1,6 +1,4 @@
 case class Matrix(l: Seq[Seq[Int]]):
    val saddlePoints: Set[(Int, Int)] =
-      val cs = l.head.indices.map(j => l.indices.map(i => l(i)(j)))
-      val maxIdx = l.indices.map(i => l(i).indices.filter(j => l(i)(j) == l(i).max))
-
-      l.indices.flatMap(i => maxIdx(i).filter(j => l(i)(j) == cs(j).min).map((i, _))).toSet
+      val cols = l.head.indices.map(j => l.map(_(j)))
+      l.indices.flatMap(i => l(i).indices.filter(j => l(i)(j) == l(i).max && l(i)(j) == cols(j).min).map((i, _))).toSet
