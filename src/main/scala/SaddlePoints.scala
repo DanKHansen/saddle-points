03e286619bac0e68eee16a325b60578c20637e8f
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-25 15:54:14 +0200

    solution

--- src/main/scala/SaddlePoints.scala
+++ src/main/scala/SaddlePoints.scala
@@ -1,2 +1,6 @@
-case class Matrix(l: List[List[Int]]):
-   val saddlePoints: Set[(Int, Int)] = ???
+case class Matrix(l: Seq[Seq[Int]]):
+   val saddlePoints: Set[(Int, Int)] =
+      val cs = l.head.indices.map(j => l.indices.map(i => l(i)(j)))
+      val maxIdx = l.indices.map(i => l(i).indices.filter(j => l(i)(j) == l(i).max))
+
+      l.indices.flatMap(i => maxIdx(i).filter(j => l(i)(j) == cs(j).min).map((i, _))).toSet
